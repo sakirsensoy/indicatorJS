5d1b00a7df654657ef49fbd6308f7b64cd28ab15
Şakir Şensoy <sakirsensoy@gmail.com>
2014-09-28 03:40:39 +0300

    - fullscreen indicator için metodlar eklendi - bazı stiller eklendi - loading animation değiştirildi

--- indicator.js
+++ indicator.js
@@ -5,12 +5,12 @@
  * @depend jQuery
  * @return object
  */
- 
+
 var indicator = function() {
 
 	/**
 	 * Remove hash and dot, from element name.
-	 * 
+	 *
 	 * @param  string elementId
 	 * @return string
 	 */
@@ -25,12 +25,12 @@ var indicator = function() {
 
 		/**
 		 * Create temporarily layer for indicator
-		 *  
+		 *
 		 * @param  string elementId
 		 * @return void
 		 */
 		show: function(elementId) {
-			
+
 			//dom
 			el = $(elementId);
 
@@ -45,15 +45,29 @@ var indicator = function() {
 				width: el.outerWidth(),
 				height: el.outerHeight()
 			})
-			.addClass('indicator indicator-loader');
+			.addClass('indicator indicator-loader')
+			.html('<div class="loading-pane">\
+			        <div class="loading-message">\
+			            <div class="spinner">\
+			                <div class="double-bounce1"></div>\
+			                <div class="double-bounce2"></div>\
+			            </div>\
+			        </div>\
+			    </div>\
+		    ');
 
 			//indicator append to element
 			el.append(tempIndicator);
+
+			// spinner margin-top
+			$('#' + elementNameFixer(elementId) + '-indicator').find('.spinner').css('margin-top', (el.outerHeight() - 40) / 2);
+
+			// console.log('#' + $(elementNameFixer(elementId) + '-indicator').find('.spinner'));
 		},
 
 		/**
 		 * Destroy loading indicator
-		 * 
+		 *
 		 * @param  string elementId
 		 * @return void
 		 */
@@ -69,6 +83,49 @@ var indicator = function() {
 
 			//find indicator and destroy
 			el.find('#' + elementNameFixer(elementId) + '-indicator').remove();
+		},
+
+		/**
+		 * Create fullscreen indicator layer
+		 *
+		 * @return void
+		 */
+		showFS: function() {
+
+			//temporarily indicator layer
+			tempIndicator = $('<div />')
+			.attr('id', 'fullscreen-indicator')
+			.css({
+				width: $(window).width(),
+				height: $(window).height()
+			})
+			.addClass('indicator indicator-loader indicator-fullscreen-loader')
+			.html('<div class="loading-pane">\
+			        <div class="loading-message">\
+			            <div class="spinner">\
+			                <div class="double-bounce1"></div>\
+			                <div class="double-bounce2"></div>\
+			            </div>\
+			        </div>\
+			    </div>\
+		    ');
+
+			//indicator append to body
+			$('body').append(tempIndicator);
+
+			// spinner margin-top
+			$('#fullscreen-indicator').find('.spinner').css('margin-top', ($(window).outerHeight() - 40) / 2);
+		},
+
+		/**
+		 * Destroy fullscreen loading indicator
+		 *
+		 * @return void
+		 */
+		hideFS: function() {
+
+			//find indicator and destroy
+			$('#fullscreen-indicator').remove();
 		}
 	};
 }();
